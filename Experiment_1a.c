c81ce24617ed7b6698dc663cc5621ca9638cd68a
Arshiq-82 <arshiq.ahmed@gmail.com>
2025-10-17 10:58:43 +0530

    Create Experiment_1a.c

---
+++ Experiment_1a.c
@@ -0,0 +1,17 @@
+/*Name of Student : Arshiq Ahmed Sayyed 
+UIN 251P082
+Class : F.E. Computer Engineering 
+Div : D 
+Age : 17*/
+#include <stdio.h>
+
+int main() {
+    
+    printf("My Name : Arshiq Ahmed Sayyed\n");
+    printf("Age : 17\n");
+    printf("Class : F.E. Computer Engineering\n");
+    printf("Division : D\n");
+    printf("UIN : 251P082\n");
+    
+    return 0;
+}
